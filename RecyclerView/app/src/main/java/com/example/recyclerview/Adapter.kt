fc7398029c691709929707a730602b9ff31d25e8
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-05-01 20:59:22 -0600

    Se agrego Viewer

--- RecyclerView/app/src/main/java/com/example/recyclerview/Adapter.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Adapter.kt
@@ -6,10 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_adapter.view.*
 
-//TODO(5) Ahora a la clase Adapter le enviaremos parametros
+//TODO(5) Ahora a la clase Adapter le enviaremos parametros "var items: List<ObjetoPersona>"
 //TODO(5.1) Ahora tendra que heredar de recyclerView que este tendra como parametro una lista con la clase ViewHolder
 //TODO(5.2) Empezara a dar un chingo de errores, solo hay que implementar y crear la clase ViewHolder que esta en la lista.
-class Adapter(var items: List<ObjetoPersona>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+//TODO(18) Vamos agregar un parametro, ademas del comentario 5. "val clickListener:(ObjetoPersona)->Unit"
+class Adapter(var items: List<ObjetoPersona>,val clickListener:(ObjetoPersona)->Unit) : RecyclerView.Adapter<Adapter.ViewHolder>() {
     //TODO(1) Cree una nueva actividad EMPTY para manejar el Recycler View
 
     //TODO(7) En esta funcion, una ves obtenidos los datos del comentario 6, necesitaremos que
@@ -23,17 +24,20 @@ class Adapter(var items: List<ObjetoPersona>) : RecyclerView.Adapter<Adapter.Vie
     //TODO(9) Aqui una ves setados los valores en el comentario 6. Se le asigna una posicion en la lista a cada "objeto" o "cardView"
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //TODO(9.1) Mas bien aqui se setean los valores
-        holder.pasarValores(items[position])
+        //TODO(19.1) Como agregamos otro parametro, le pasaremos el que esta definidio en el comentario 18
+        holder.pasarValores(items[position],clickListener)
     }
 
     //TODO (6) El viewHolder recipe como parametro una vista que es lo que esta en el XML de "item0, item1..."
     class ViewHolder(var item: View) : RecyclerView.ViewHolder(item) {
     //TODO(6.1) Se crea una funcion con parametro Persona para poder asignarle los valores escritos del XML-1 al XML-2
-        fun pasarValores(persona: ObjetoPersona)=
+    //TODO(19) Aqui vamos a pasar un nuevo parametro que es el evento del click
+        fun pasarValores(persona: ObjetoPersona,eventoClick:(ObjetoPersona)->Unit)=
             with(item){
                 text_view_adapter_nombre.text=persona.nombre
                 text_view_adapter_apellido.text=persona.apellido
-
+                //TODO(20) Seteamos el evento del clickListener
+                this.setOnClickListener{eventoClick(persona)}
         }
     }
 
--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.recyclerview
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_adapter.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,10 +33,19 @@ class MainActivity : AppCompatActivity() {
             //TODO(13.1)Agregamos a la lista los valores agregados por el usuario
             listaPersona.add(ObjetoPersona(name1,lastName))
             //TODO(14) En teoria se hacae la instancia del adapter, pues se la pasamos como parametro la lista.
-            mAdapter = Adapter(listaPersona)
+            //TODO(22) Ahora le pasamos como parametro la funcion creada en el comentario 21
+            mAdapter = Adapter(listaPersona,{variable:ObjetoPersona->personaItemClicked(variable)})
             //TODO(15) Vaya ahora al recycler del XML le asignamos los valores de la dicha lista del comentario anterior.
             recycler.adapter=mAdapter
         }
-
+    }
+    //TODO(21) Ahora en el comentario 14 dara vergazo por que le agregamos un nuevo parametro.
+    //TODO (21.1) Ahora, una funcion que se encargara del evento del click con parametro del objeto
+    private fun personaItemClicked(item:ObjetoPersona){
+    //TODO(21.2) Inciamos el INTENTO
+        startActivity(Intent(this,ViewerPersona::class.java)
+                //TODO(25) Aqui se hace la sentencia PutExtra con el fin de setear
+            .putExtra("Nombre",text_view_adapter_nombre.text.toString())
+            .putExtra("Apellido",text_view_adapter_apellido.text.toString()))
     }
 }
