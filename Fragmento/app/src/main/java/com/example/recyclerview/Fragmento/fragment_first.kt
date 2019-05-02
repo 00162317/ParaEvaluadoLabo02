e48adee1d9dd0d5f82dff97fddb6b4b25c107ae4
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-05-01 21:47:57 -0600

    Lo que estaba en _"activity_viewer_persona" paso a ser el fragmento 2. Lo que estaba en "activity_main" paso a ser el Fragmento 1

--- Fragmento/app/src/main/java/com/example/recyclerview/Fragmento/fragment_first.kt
+++ Fragmento/app/src/main/java/com/example/recyclerview/Fragmento/fragment_first.kt
@@ -9,23 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.recyclerview.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [fragment_first.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [fragment_first.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class fragment_first : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
@@ -46,7 +34,7 @@ class fragment_first : Fragment() {
         return inflater.inflate(R.layout.fragment_fragment_first, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     fun onButtonPressed(uri: Uri) {
         listener?.onFragmentInteraction(uri)
     }
@@ -65,32 +53,13 @@ class fragment_first : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
+
     interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+
         fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_first.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             fragment_first().apply {
